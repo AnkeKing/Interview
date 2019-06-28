58d1614d58dde6bb84f36dcf3b18a6e6be269bbd
AnkeKing <2678924540@qq.com>
2019-06-28 23:02:26 +0800

    深拷贝未完

--- js/compatibility.js
+++ js/compatibility.js
@@ -3,4 +3,13 @@ if($(".box-one").html().length>30){
     var str=$(".box-one").html().substring(0,30);
     $(".box-one").html(str+"...");
 }
-//
+//2.深拷贝
+var obj={
+    name:"Cindy",
+    age:19
+}
+var arr=[1,1,2,3];
+function fun(){
+    return "fun"
+}
+console.log(typeof arr.splice=="function");
